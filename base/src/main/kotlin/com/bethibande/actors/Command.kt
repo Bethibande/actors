c703929b99d8d44b8188ca215d37dfc07d426e1b
Max <bethibande@gmail.com>
2024-02-29 10:05:27 +0100

    start it-2

--- base/src/main/kotlin/com/bethibande/actors/Command.kt
+++
@@ -1,7 +0,0 @@
-package com.bethibande.actors
-
-/**
- * Commands are used to interact with actors, all commands must be serializable.
- * Commands are sent to actors to perform a certain action.
- */
-interface Command
--- base/src/main/kotlin/com/bethibande/actors/exception/InvalidKeyException.kt
+++
@@ -1,5 +0,0 @@
-package com.bethibande.actors.exception
-
-import com.bethibande.actors.ActorKey
-
-class InvalidKeyException(key: ActorKey<*>): RuntimeException("Invalid key $key")
