e9742e3eabde1e44f7c8a020650908213d5eb733
Bethibande <bethibande@gmail.com>
2024-03-02 16:04:14 +0100

    chore: Make BehaviorMap thread-safe #1

--- lib/src/main/kotlin/com/bethibande/actors/behavior/BehaviorMap.kt
+++ lib/src/main/kotlin/com/bethibande/actors/behavior/BehaviorMap.kt
@@ -1,6 +1,9 @@
 package com.bethibande.actors.behavior
 
 import java.util.Objects
+import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.read
+import kotlin.concurrent.write
 
 /**
  * A behavior map representing a configurable list of behaviors.
@@ -11,14 +14,17 @@ import java.util.Objects
  * The BehaviorMap is backed by a HashMap.
  * It cannot route commands based on superclasses of the command.
  *
+ * !! Important: this map is synchronized using a ReentrantReadWriteLock and could possibly lock actor coroutines when modified.
+ *
  * @param C root command interface used by the actor
  * @param S state class used for the actor
  */
 class BehaviorMap<C, S>: Behavior<C, S> {
 
+    private val lock = ReentrantReadWriteLock()
     private val behaviors = HashMap<Class<out C>, Behavior<out C, S>>()
 
-    fun <T: C> add(commandType: Class<T>, behavior: Behavior<T, S>) {
+    fun <T: C> add(commandType: Class<T>, behavior: Behavior<T, S>) = lock.write {
         behaviors[commandType] = behavior
     }
 
@@ -27,8 +33,8 @@ class BehaviorMap<C, S>: Behavior<C, S> {
     }
 
     @Suppress("UNCHECKED_CAST")
-    fun <T: C> get(commandType: Class<T>): Behavior<T, S>? {
-        return behaviors[commandType] as? Behavior<T, S>
+    fun <T: C> get(commandType: Class<T>): Behavior<T, S>? = lock.read {
+        behaviors[commandType] as? Behavior<T, S>
     }
 
     @Suppress("UNCHECKED_CAST")
